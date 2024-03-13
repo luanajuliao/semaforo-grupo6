d636082f7f01a97d92d2f780a6e182499f97af17
Luana Julião <128929284+luanajuliao@users.noreply.github.com>
2024-03-13 15:16:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,60 @@
 const $luzesDoCirculo = document.querySelectorAll('.luz-circulo');
 let contadorDeLuz = 0;
 let intervalo;
-let semaforo =0;
+let semaforo = 0;
+
+// Carregar os áudios
+let audioAtravesse = new Audio('audio/Atravesse.mp3');
+let audioAtenção = new Audio('audio/Atenção.mp3');
+let audioEspere = new Audio('audio/Espere.mp3');
 
 function on() {
-    if( semaforo == 0){
+    if (semaforo == 0) {
         semaforo = 1;
-    }else{
+    } else {
         semaforo = 0;
     }
 }
 
-const mostrarLuz = () =>{
+const mostrarLuz = () => {
+    // Parar todos os áudios antes de trocar de cor
+    audioAtravesse.pause();
+    audioAtenção.pause();
+    audioEspere.pause();
+
     $luzesDoCirculo[contadorDeLuz].className = 'luz-circulo';
     contadorDeLuz--;
 
-    if(contadorDeLuz < 0) contadorDeLuz = 2;
+    if (contadorDeLuz < 0) contadorDeLuz = 2;
 
-    const luzActual = $luzesDoCirculo[contadorDeLuz];
-    luzActual.classList.add(luzActual.getAttribute('color'))
+    const luzAtual = $luzesDoCirculo[contadorDeLuz];
+    luzAtual.classList.add(luzAtual.getAttribute('color'));
+
+    // Reproduzir o áudio correspondente à cor atual
+    if (luzAtual.classList.contains('green')) {
+        audioAtravesse.play();
+    } else if (luzAtual.classList.contains('yellow')) {
+        audioAtenção.play();
+    } else if (luzAtual.classList.contains('red')) {
+        audioEspere.play();
+    }
 }
+
 const boton = document.getElementById('button');
-        boton.addEventListener('click', () => {
-            if (on == 1 ) {
-                clearInterval(intervalo);
-                intervalo = null;
-                boton.querySelector('button').textContent = 'Ligar';
-            } else {
-                intervalo = setInterval(mostrarLuz,2000);
-                boton.querySelector('button').textContent = 'Desligar';
-            }
-        });
+boton.addEventListener('click', () => {
+    if (semaforo == 1) {
+        clearInterval(intervalo);
+        $luzesDoCirculo.forEach(luz => luz.classList.remove('green', 'yellow', 'red'));
+        $luzesDoCirculo[0].classList.add('red');
+        semaforo = 0;
+        boton.textContent = 'Ligar';
+        boton.classList.remove('button'); // remova a classe de estilo para desligado
+        boton.classList.add('button'); // adicione a classe de estilo para desligado
+    } else {
+        intervalo = setInterval(mostrarLuz, 4000); // Mudança de cor a cada x segundos
+        semaforo = 1;
+        boton.textContent = 'Desligar';
+        boton.classList.remove('button'); // remova a classe de estilo para desligado
+        boton.classList.add('button'); // adicione a classe de estilo para desligado
+    }
+});
